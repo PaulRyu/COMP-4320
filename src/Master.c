2225d0da4a668fef9998e79863ca08f2dbdc86e6
PaulRyu <pauliedho@gmail.com>
2018-10-23 12:48:38 -0500

    Begin Master.c path, prepare for Master.java if not.

--- src/Master.c
+++ src/Master.c
@@ -1,1 +1,27 @@
-// Need to implement
+/* Course: COMP-4320 Introduction to Networks
+ * Institution: Auburn University
+ * Submitting: Laboratory Assignment 2
+ * Date: October 25, 2018
+ * Authors: Paul Ryu, William Atkinson, Abriana Fornis
+ * Emails: phc0004@auburn.edu, wja0007@auburn.edu, adf0018@auburn.edu
+ * Resources used: JetBrains CLion IDE, Github, Sublime Text 3
+ *
+ * Description: The objective of this lab is to create a virtual ring of nodes over the Internet. After joining
+ *             the ring, the nodes on the ring MUST use only the ring to communicate. The ring is managed by a
+ *             master which is part of the ring and has Ring ID 0 (zero). All the other nodes of the ring are slave
+ *             nodes (clients) with a ring ID assigned by the master.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <netdb.h>
+
+#define MAGIC_NUMBER = 0x1234
+
+int sockfd, numbytes, rv, masterPortNumber;
