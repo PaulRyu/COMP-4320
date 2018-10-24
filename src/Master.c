686a5a7dbd6591d4830a4fd737a4ca8c9f756606
AbrianaDF <adf0018@auburn.edu>
2018-10-24 13:01:56 -0500

    Master may handle connections

--- src/Master.c
+++ src/Master.c
@@ -27,21 +27,22 @@
 
 #define PORTNUM "10120" // needs to change for command line function
 #define BACKLOG 10
+
 int main(int argc, char *argv[]) {
 
-    int myRID = 0; // ring ID for master
-    int slaveRID = 1; // assign & increment whenever a node joins
-    int myGID = 22;
+    int ringID = 0; // ring ID for master
+    int nextRID = 1; // assign & increment whenever a node joins
+    // int myGID = 22;
     unsigned const int magic_number = 0x4A6F7921;
     const char* portNum;
     int packed_clientIP = -1;
 
-    struct sockaddr_storage their_addr;
     socklen_t addr_size;
-    struct addrinfo hints, *res;
+    struct addrinfo hints, *result;
+    struct sockaddr_in slave_info, *nextSlaveIP;
     int sockfd, new_fd;
 
-    // insert error checking here
+    // check command line arguments
     if (argc == 2) {
         portNum = argv[1];
     }
@@ -60,21 +61,43 @@ int main(int argc, char *argv[]) {
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE; // fills in my IP address automatically
 
-    getaddrinfo(NULL, portNum, &hints, &res);
-
+    int status;
+    if (status = getaddrinfo(NULL, portNum, &hints, &result) != 0) {
+        printf("Something went wrong with call to getaddrinfo(): \n", gai_strerror(status));
+        exit(1);
+    }
+    // assign Master IP address to nextSlaveIP
+    //nextSlaveIP->
     // create socket, bind it, and listen on it
-
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    sockfd = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
     printf("Socket created.\n");
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
-    printf("Master bound to socket\n");
-    listen(sockfd, BACKLOG);
 
-    printf("Master listening on port ", portNum);
+    if (bind(sockfd, result->ai_addr, result->ai_addrlen) < 0) {
+        printf("Error: bind failed!");
+        exit(1);
+    }
+
+    printf("Master: bound to socket\n");
+    if (listen(sockfd, BACKLOG) < 0) {
+        printf("Error: Something went wrong listening on port ", portNum);
+        exit(1);
+    } else {
+        printf("Master: listening on port %s", portNum, "\n");
+    }
+
     // accepting incoming connections
+    while (true) {
+        printf("Master: waiting for a connection");
+        addr_size = sizeof slave_info;
+        new_fd = accept(sockfd, (struct sockaddr *)&slave_info, &addr_size);
+        if (new_fd < 0) {
+            printf("Error: Could not connect to client!");
+            exit(1);
+        }
+        
+        printf("Master: received connection from %s", inet_ntoa(slave_info.sin_addr));
+    }
 
-    //while (true) {
-
-    //}
+    freeaddrinfo(result); 
     return 0;
 }
